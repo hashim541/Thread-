0a7b17322d56ffda0c6f32b696e53a77cbfdf964
hashim <hashim00541x@gmail.com>
2024-06-11 17:00:29 +0530

    updating nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-        serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'img.clerk.com'
-            },
-            {
-              protocol: "https",
-              hostname: "images.clerk.dev",
-            },
-            {
-              protocol: "https",
-              hostname: "uploadthing.com",
-            },
-            {
-              protocol: "https",
-              hostname: "placehold.co",
-            },
-        ]
-    }
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  experimental: {
+      serverActions: true,
+      serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'img.clerk.com'
+          },
+          {
+            protocol: "https",
+            hostname: "images.clerk.dev",
+          },
+          {
+            protocol: "https",
+            hostname: "uploadthing.com",
+          },
+          {
+            protocol: "https",
+            hostname: "placehold.co",
+          },
+      ]
+  }
 };
 
 export default nextConfig;
