b9f394535efaf81b47647e6f88412e9846ce6211
hashim <hashim00541x@gmail.com>
2024-06-20 14:28:02 +0530

    added dm mono font

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={DM_mono.className}>
           <TopBar/>
           <main className="flex flex-row">
             <LeftSideBar/>
